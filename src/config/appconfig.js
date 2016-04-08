f63bd2a44d13d64bbf8ba652b5b189fbe74244b4
Vaibhaw_Office <vaibhawraj@yahoo.in>
2016-04-08 17:02:25 +0530

    Files Updated

---
+++ src/config/appconfig.js
@@ -0,0 +1,18 @@
+/* @if NODE_ENV='development' */
+{
+	"loginUrl": "https://login.salesforce.com/",
+	"clientId": "3MVG9ZL0ppGP5UrATwJpA7dkv1tjJDE1.rh.ni1igPMGrnn6TZHCeSfRzpg8LEJRe9QiOCJKCQfcthn4ojD2S",
+	"redirectUri": "http://localhost:8080/sfapp/oauthcallback.html",
+	"proxyUrl": "http://localhost:8080/sfapp/proxy.php?mode=native",
+	"api_version": "v34.0"
+}
+/* @endif */
+/* @if NODE_ENV='apk' */
+{
+	"loginUrl": "https://login.salesforce.com/",
+	"clientId": "3MVG9ZL0ppGP5UrATwJpA7dkv1t5KUC_IO49hwGCrLifucumGq.mmxZPOjbg59RUsQXtqrM8Xi_uOfsW_zUYj",
+	"redirectUri": "http://localhost:8080/sfapp/oauthcallback.html",
+	"proxyUrl": null,
+	"api_version": "v34.0"
+}
+/* @endif */
