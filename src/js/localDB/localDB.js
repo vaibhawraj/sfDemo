6d2ad8275b8d83b9160c69975464984b4f94b3e6
Vaibhaw <vaibhawraj@yahoo.in>
2016-04-10 02:06:24 +0530

    UpdateRow method added to localDB

--- src/js/localDB/localDB.js
+++ src/js/localDB/localDB.js
@@ -44,7 +44,7 @@ define(['underscore'],function(_){
 			log.info('Table ' + name + ' created');
 		},
 		insertRow : function(table,rec){
-			if(!(_.has(this._storage,table) && _.isObject(rec)))
+			if(!this.isTableExist(table) && _.isObject(rec))
 				return;
 			var record = {};
 			_.each(this._storage[table].fields,function(element,index,list){
@@ -57,10 +57,37 @@ define(['underscore'],function(_){
 			this._storage[table].count += 1
 			this._storage[table].records.push(record);
 		},
-		updateRow : function(){},
+		updateRow : function(table,rec){
+			if(!this.isTableExist(table) && _.isObject(rec)) return false;
+			var updateStatus = false;
+			var errorMsg = '';
+			if(_.has(rec,'uuid')) {
+				_.each(this._storage[table].records,function(element,index,list){
+					if(element.uuid == rec.uuid) {
+						var temp = element;
+						//For each key in rec, assign new value to temprory record
+						//Replace previous entry with new entry
+						this._storage[table].records[index] = temp;
+						updateStatus=true;
+					}
+				});
+				return updateStatus;
+			} else {
+				log.error('UUID is not specified for table "'+table + '" : Record ' + rec);
+				return false;
+			}
+		},
 		queryRow : function(){},
 		purgeTable : function(){},
 		deleteRow : function(){},
+
+		isTableExist : function(table){
+			if(!this._dbLoaded) {
+				log.error('No database loaded');
+				return false;
+			}
+			return _.has(this._storage,table);
+		},
 		commit : function(){
 			if(this._storage == null || this._name == null) {
 				log.error('No database loaded',this._name,this._storage);
