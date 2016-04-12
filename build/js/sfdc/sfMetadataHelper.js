1b29a7dad146ef8f18c2cea5a62aed277c2511b7
Vaibhaw_Office <vaibhawraj@yahoo.in>
2016-04-12 19:01:08 +0530

    Create Screen Initialized

--- build/js/sfdc/sfMetadataHelper.js
+++ build/js/sfdc/sfMetadataHelper.js
@@ -1,6 +1,10 @@
 define(['localDB','json!mapping','networkManager'],function(localDB,mapping,nm){
 	var sfMetadataHelper = {
-		init : function(){}
+		init : function(){},
+		getPicklistValues : function(field){
+			log.debug(field);
+			return ['Option 1','My Name is Khan','You are my hero','FOO'];
+		}
 	};
 	if(_.isUndefined(window.sfDataMetadataHelper)){
 		window.sfMetadataHelper = sfMetadataHelper;
--- src/js/controller/homeController.js
+++ src/js/controller/homeController.js
@@ -85,6 +85,20 @@ define(['networkManager','sfMetadataHelper','json!mapping'],function(nm,sfMetada
 			$scope.reRenderHome();
 			$('body').pagecontainer("change","#home",{changeHash:false,transition:"slide",reverse:true});
 		}
+		$scope.gotoNewPage = function(reset) {
+			if(!_.isUndefined(reset))
+			{
+				if(reset) $scope.reset();
+			}
+			$('body').pagecontainer("change","#newPage",{changeHash:false,transition:"slide"});
+		}
+		$scope.gotoPicklistScreen = function(field){
+				if($scope.picklistHelper.init(field))
+				{
+					$('body').pagecontainer("change","#picklist",{changeHash:false,transition:"slide"});
+				}
+			
+		}
 
 		//Detail View Part
 		$scope.record = {};
@@ -95,5 +109,40 @@ define(['networkManager','sfMetadataHelper','json!mapping'],function(nm,sfMetada
 			if(_.isUndefined(field)) return 'NaN';
 			return field.form_label;
 		}
+		//New Page
+		$scope.newRecord = {};
+		$scope.reset = function(){
+			$scope.newRecord = {};
+		}
+
+		//Picklist Helper
+		$scope.picklistHelper = {
+			selectedValue : null,
+			field : null,
+			picklistValues : [],
+			select : function(value){
+				this.selectedValue = value;
+				if(!_.isNull(this.field)) {
+					$scope.newRecord[field] = selectedValue;
+				}
+				//Return to page
+				$('body').pagecontainer("change","#newPage",{changeHash:false,transition:"slide",reverse:true});
+				if(_.isNull($scope.$$phase)) $scope.$apply();
+			},
+			init:function(field){
+				var sfField = _.find(mapping.fields,function(f){return f.table_api == field;});
+				if(!_.isUndefined(sfField)){
+					this.picklistValues = sfMetadataHelper.getPicklistValues(sfField.sf_api);
+					this.selectedValue = null;
+					this.field = field;
+					if(_.isNull($scope.$$phase)){
+						$scope.$apply();
+					}
+					$("#picklistview").listview();
+					$("#picklistview").listview("refresh");
+					return true;
+				} else return false;
+			}
+		};
 	}
 });
